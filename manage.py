f9602915c2fb174a830ef2ba9ad604abc698fd32
derrickmacharia <derrick.macharia@student.moringaschool.com>
2021-11-07 18:08:49 +0300

    done initial migration

--- manage.py
+++ manage.py
@@ -25,7 +25,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Post=Post, Category=Category, Comment=Comment)
+    return dict(app=app, db=db, User=User, Postpitches=Post, Category=Category, Comment=Comment)
 
 
 if __name__=='__main__':
